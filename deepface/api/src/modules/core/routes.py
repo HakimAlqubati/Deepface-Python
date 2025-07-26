17b8b4f609a3e067832acfdd350d946a41b1cea4
HakimAlqubati <hakimahmed123321@gmail.com>
2025-07-27 02:51:13 +0300

    Update routes.py

--- deepface/api/src/modules/core/routes.py
+++ deepface/api/src/modules/core/routes.py
@@ -337,9 +337,9 @@ def recognize_v2():
         matches_below_threshold = [d for d in distances if d < DISTANCE_THRESHOLD]
 
         # شرط المطابقة
-        if (REQUIRE_MULTI_MATCH and len(matches_below_threshold) >= 3) or \
-           (not REQUIRE_MULTI_MATCH and len(matches_below_threshold) >= 1):
-        # if (1==1)
+        # if (REQUIRE_MULTI_MATCH and len(matches_below_threshold) >= 3) or \
+        #    (not REQUIRE_MULTI_MATCH and len(matches_below_threshold) >= 1):
+        if (1==1)
 
             min_distance = min(matches_below_threshold)
             if min_distance < best_distance:
